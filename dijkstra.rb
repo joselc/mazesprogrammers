817a84d463fbc70c9403992bf0e7da0cb058a9ac
José López Calleja <jose.lopez226@gmail.com>
2017-06-02 19:15:52 +0200

    MP-DIJKSTRA: implemented shortest path method

--- dijkstra.rb
+++ dijkstra.rb
@@ -1,11 +1,17 @@
 require 'distance_grid'
 require 'binary_tree'
+require 'sidewinder'
 
-grid = DistanceGrid.new(5, 5)
-BinaryTree.on(grid)
+grid = DistanceGrid.new(10, 10)
+Sidewinder.on(grid)
 
 start = grid[0,0]
 distances = start.distances
 
 grid.distances = distances
 puts grid
+
+puts "path from northwest corner to southwest corner"
+grid.distances = distances.path_to(grid[grid.rows - 1, 0])
+puts grid.to_s
+
